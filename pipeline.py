c30008f981e2824602be048a02261a7639890902
khayyam <khayyam@gmail.com>
2025-12-12 01:01:06 +0400

    third

--- pipeline.py
+++ pipeline.py
@@ -1,32 +1,18 @@
-# import os
-# from google.oauth2.service_account import Credentials
-# from googleapiclient.discovery import build
-# from config import sheets_id, scopes
+import os
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from config import sheets_id, scopes
 
-# service_account_file = 'keys.json'
+service_account_file = 'keys.json'
 
-# credentials = Credentials.from_service_account_file(service_account_file, scopes=scopes)
+credentials = Credentials.from_service_account_file(service_account_file, scopes=scopes)
 
-# service = build('sheets', 'v4', credentials=credentials)
-# sheet = service.spreadsheets()
+service = build('sheets', 'v4', credentials=credentials)
+sheet = service.spreadsheets()
 
-# range = "A1:Z500"
-# result = sheet.values().get(spreadsheetId=sheets_id, range=range).execute()
+range = "A1:Z500"
+result = sheet.values().get(spreadsheetId=sheets_id, range=range).execute()
 
-# data = result.get("values", [])
+data = result.get("values", [])
 
-# print(data)
-
-
-from read_data import read_sheet
-from write_data import write_csv, write_excel
-
-def main():
-    data = read_sheet("A1:Z500")
-    print("Sheet-dən data oxundu:", data)
-
-    write_csv(data, "local_data.csv")
-    write_excel(data, "local_data.xlsx")
-
-if __name__ == "__main__":
-    main()
+print(data)
